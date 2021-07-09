4f19d3d9df829bb7a1f2117a1975ea65d1c4e623
Purab <purab.bak@gmail.com>
2021-07-10 00:44:39 +0530

    Updating meeting details to firestore

--- app/src/main/java/com/lite/housepartynew/Activities/ReminderActivity.java
+++ app/src/main/java/com/lite/housepartynew/Activities/ReminderActivity.java
@@ -16,9 +16,14 @@ import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.lite.housepartynew.Adapters.EmailListAdapter;
+import com.lite.housepartynew.Models.Meeting;
 import com.lite.housepartynew.R;
 
 import java.text.ParseException;
@@ -30,14 +35,14 @@ import java.util.List;
 
 public class ReminderActivity extends AppCompatActivity {
 
-    EditText eventNameEt, eventLocationEt, eventDescEt;
+    EditText eventNameEt, eventDescEt;
     Button addEventBtn;
 
 
-//    Button btnDatePicker, btnTimePicker;
-//    EditText txtDate, txtTime;
-//    private int mYear, mMonth, mDay, mHour, mMinute;
-//    String sDate1="";
+    Button btnDatePicker, btnTimePicker;
+    EditText txtDate, txtTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    String sDate1 = "";
 
     List<String> emailIdsList;
     RecyclerView emailListRV;
@@ -46,6 +51,12 @@ public class ReminderActivity extends AppCompatActivity {
     EditText recipientEmailET;
 
     DatabaseReference scheduledMeetingsRef;
+
+    FirebaseUser mCurrentUser;
+
+    FirebaseFirestore firestoreDb;
+
+    String date, time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,38 +68,7 @@ public class ReminderActivity extends AppCompatActivity {
         addEventBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                StringBuilder emailIntent = new StringBuilder();
-
-                for (String s : emailIdsList){
-                    emailIntent.append(s+", ");
-                }
-
-                if (!eventNameEt.getText().toString().isEmpty() &&
-                        !eventDescEt.getText().toString().isEmpty() &&
-                        !eventLocationEt.getText().toString().isEmpty() && !emailIntent.toString().isEmpty()) {
-
-                    Intent intent = new Intent(Intent.ACTION_INSERT);
-                    intent.setData(CalendarContract.Events.CONTENT_URI);
-                    intent.putExtra(CalendarContract.Events.TITLE, eventNameEt.getText().toString());
-                    intent.putExtra(CalendarContract.Events.EVENT_LOCATION, eventLocationEt.getText().toString());
-                    intent.putExtra(CalendarContract.Events.DESCRIPTION, eventDescEt.getText().toString());
-                    intent.putExtra(CalendarContract.Events.ALL_DAY, true);
-                    intent.putExtra(Intent.EXTRA_EMAIL, emailIntent.toString());
-
-                    if (intent.resolveActivity(getPackageManager()) != null){
-                       // updateToDatabase();
-                        startActivity(intent);
-                    }
-                    else {
-                        showToast("There is no app that supports this action");
-                    }
-
-
-                }
-                else {
-                    showToast("Fields cannot be empty");
-                }
+                addEvent();
             }
         });
 
@@ -96,27 +76,164 @@ public class ReminderActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                addEmailToRV();
+            }
+        });
 
-                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickDate();
+            }
+        });
 
-                if (!recipientEmailET.getText().toString().isEmpty()){
+        btnTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                    if (!recipientEmailET.getText().toString().trim().matches(emailPattern)) {
-                        Toast.makeText(getApplicationContext(), "Invalid email address", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        showToast("adding to the list");
-                        emailIdsList.add(recipientEmailET.getText().toString());
-                        recipientEmailET.setText("");
-                    }
-                }
-                else{
-                    showToast("Email can't be empty");
+                if (sDate1.isEmpty()) {
+                    showToast("Select a date first");
+                } else {
+                    pickTime();
                 }
             }
         });
 
+    }///oncreate ends here
 
+
+    private void addEmailToRV() {
+
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (!recipientEmailET.getText().toString().isEmpty()) {
+
+            if (!recipientEmailET.getText().toString().trim().matches(emailPattern)) {
+                Toast.makeText(getApplicationContext(), "Invalid email address", Toast.LENGTH_SHORT).show();
+            } else {
+                showToast("adding to the list");
+                emailIdsList.add(recipientEmailET.getText().toString());
+                recipientEmailET.setText("");
+            }
+        } else {
+            showToast("Email can't be empty");
+        }
+    }
+
+    private void addEvent() {
+
+        StringBuilder emailIntent = new StringBuilder();
+
+        for (String s : emailIdsList) {
+            emailIntent.append(s + ", ");
+        }
+
+        if (!eventNameEt.getText().toString().isEmpty() &&
+                !eventDescEt.getText().toString().isEmpty() &&
+                !emailIntent.toString().isEmpty() && !time.isEmpty()) {
+
+            try {
+                Date date1 = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").parse(sDate1);
+                txtDate.setText(date1.toString());
+
+                long epoch = date1.getTime();
+                txtDate.setText(String.valueOf(epoch));
+
+                Intent intent = new Intent(Intent.ACTION_INSERT);
+                intent.setData(CalendarContract.Events.CONTENT_URI);
+                intent.putExtra(CalendarContract.Events.TITLE, eventNameEt.getText().toString());
+                intent.putExtra(CalendarContract.Events.DESCRIPTION, eventDescEt.getText().toString());
+                intent.putExtra(CalendarContract.Events.ALL_DAY, true);
+                intent.putExtra(Intent.EXTRA_EMAIL, emailIntent.toString());
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    //updateToDatabase();
+
+                    Meeting meeting = new Meeting(mCurrentUser.getEmail(), eventNameEt.getText().toString(), emailIdsList, String.valueOf(epoch), eventDescEt.getText().toString());
+
+                    firestoreDb.collection("meetings").document(eventNameEt.getText().toString()).set(meeting)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+
+                                    showToast("Meeting created! Added to database");
+                                    startActivity(intent);
+
+                                }
+                            });
+
+                    gotoDashboard();
+                } else {
+                    showToast("There is no app that supports this action");
+                }
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+
+        } else {
+            showToast("Fields cannot be empty");
+        }
+    }
+
+    private void gotoDashboard() {
+
+        startActivity(new Intent(ReminderActivity.this, DashboardActivity.class));
+        finish();
+
+    }
+
+    private void pickTime() {
+
+        time = "";
+
+        // Get Current Time
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        // Launch Time Picker Dialog
+        TimePickerDialog timePickerDialog = new TimePickerDialog(ReminderActivity.this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+
+                        txtTime.setText(hourOfDay + ":" + minute);
+
+                        //HH:mm:ss
+
+                        time = hourOfDay + ":" + minute + ":00";
+                        sDate1 = sDate1 + time;
+                    }
+                }, mHour, mMinute, false);
+        timePickerDialog.show();
+    }
+
+    private void pickDate() {
+        sDate1 = "";
+
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(ReminderActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                        sDate1 = dayOfMonth + "-" + (monthOfYear + 1) + "-" + year + " ";
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
     }
 
     private void showToast(String s) {
@@ -126,14 +243,13 @@ public class ReminderActivity extends AppCompatActivity {
     private void initUI() {
 
         eventNameEt = findViewById(R.id.eventNameEt);
-        eventLocationEt = findViewById(R.id.eventLocationEt);
         eventDescEt = findViewById(R.id.eventDescEt);
         addEventBtn = findViewById(R.id.addEventButton);
 
-//        btnDatePicker=(Button)findViewById(R.id.btn_date);
-//        btnTimePicker=(Button)findViewById(R.id.btn_time);
-//        txtDate=(EditText)findViewById(R.id.in_date);
-//        txtTime=(EditText)findViewById(R.id.in_time);
+        btnDatePicker = (Button) findViewById(R.id.btn_date);
+        btnTimePicker = (Button) findViewById(R.id.btn_time);
+        txtDate = (EditText) findViewById(R.id.in_date);
+        txtTime = (EditText) findViewById(R.id.in_time);
 
         addEmailButton = findViewById(R.id.addEmailButton);
         recipientEmailET = findViewById(R.id.recipientEmailET);
@@ -147,104 +263,44 @@ public class ReminderActivity extends AppCompatActivity {
 
         scheduledMeetingsRef = FirebaseDatabase.getInstance().getReference().child("scheduled-meetings");
 
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        firestoreDb = FirebaseFirestore.getInstance();
+
     }
 
     public void onBackClicked(View view) {
         super.onBackPressed();
     }
 
-    /**Date time not working**/
-    /*{
-        try {
-            Date date1=new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").parse(sDate1);
-            txtDate.setText(date1.toString());
+//    /**Date time not working**/
+//    {
+//        try {
+//            Date date1=new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").parse(sDate1);
+//            txtDate.setText(date1.toString());
+//
+//            long epoch = date1.getTime();
+//            txtDate.setText(String.valueOf(epoch));
+//
+//            Intent intent = new Intent(Intent.ACTION_INSERT);
+//            intent.setData(CalendarContract.Events.CONTENT_URI);
+//            intent.putExtra(CalendarContract.Events.TITLE, eventNameEt.getText().toString());
+//            intent.putExtra(CalendarContract.Events.EVENT_LOCATION, eventLocationEt.getText().toString());
+//            intent.putExtra(CalendarContract.Events.DESCRIPTION, eventDescEt.getText().toString());
+//            intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, String.valueOf(epoch));
+//            intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, String.valueOf(epoch+200));
+//
+//            if (intent.resolveActivity(getPackageManager()) != null){
+//                startActivity(intent);
+//            }
+//            else {
+//                showToast("There is no app that supports this action");
+//            }
+//
+//
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-            long epoch = date1.getTime();
-            txtDate.setText(String.valueOf(epoch));
-
-            Intent intent = new Intent(Intent.ACTION_INSERT);
-            intent.setData(CalendarContract.Events.CONTENT_URI);
-            intent.putExtra(CalendarContract.Events.TITLE, eventNameEt.getText().toString());
-            intent.putExtra(CalendarContract.Events.EVENT_LOCATION, eventLocationEt.getText().toString());
-            intent.putExtra(CalendarContract.Events.DESCRIPTION, eventDescEt.getText().toString());
-            intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, String.valueOf(epoch));
-            intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, String.valueOf(epoch+200));
-
-            if (intent.resolveActivity(getPackageManager()) != null){
-                startActivity(intent);
-            }
-            else {
-                showToast("There is no app that supports this action");
-            }
-
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
-
-            btnDatePicker.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            sDate1="";
-
-            // Get Current Date
-            final Calendar c = Calendar.getInstance();
-            mYear = c.get(Calendar.YEAR);
-            mMonth = c.get(Calendar.MONTH);
-            mDay = c.get(Calendar.DAY_OF_MONTH);
-
-
-            DatePickerDialog datePickerDialog = new DatePickerDialog(ReminderActivity.this,
-                    new DatePickerDialog.OnDateSetListener() {
-
-                        @Override
-                        public void onDateSet(DatePicker view, int year,
-                                              int monthOfYear, int dayOfMonth) {
-
-                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
-                            sDate1 = dayOfMonth + "-" + (monthOfYear + 1) + "-" + year+" ";
-                        }
-                    }, mYear, mMonth, mDay);
-            datePickerDialog.show();
-
-
-        }
-    });
-
-        btnTimePicker.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            if (sDate1.isEmpty()){
-                showToast("Select a date first");
-            }
-            else{
-
-                // Get Current Time
-                final Calendar c = Calendar.getInstance();
-                mHour = c.get(Calendar.HOUR_OF_DAY);
-                mMinute = c.get(Calendar.MINUTE);
-
-                // Launch Time Picker Dialog
-                TimePickerDialog timePickerDialog = new TimePickerDialog(ReminderActivity.this,
-                        new TimePickerDialog.OnTimeSetListener() {
-
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay,
-                                                  int minute) {
-
-                                txtTime.setText(hourOfDay + ":" + minute);
-
-                                //HH:mm:ss
-                                sDate1=sDate1+hourOfDay+":"+minute+":00";
-                            }
-                        }, mHour, mMinute, false);
-                timePickerDialog.show();
-
-            }
-
-        }
-    });*/
 }
---
+++ app/src/main/java/com/lite/housepartynew/Models/Meeting.java
@@ -0,0 +1,63 @@
+package com.lite.housepartynew.Models;
+
+import java.util.List;
+
+public class Meeting {
+
+    String hostEmail;
+    String meetingId;
+    List<String> participantsEmailList;
+    String timeUTC;
+    String description;
+
+    public Meeting() {
+    }
+
+    public Meeting(String hostEmail, String meetingId, List<String> participantsEmailList, String timeUTC, String description) {
+        this.hostEmail = hostEmail;
+        this.meetingId = meetingId;
+        this.participantsEmailList = participantsEmailList;
+        this.timeUTC = timeUTC;
+        this.description = description;
+    }
+
+    public String getHostEmail() {
+        return hostEmail;
+    }
+
+    public void setHostEmail(String hostEmail) {
+        this.hostEmail = hostEmail;
+    }
+
+    public String getMeetingId() {
+        return meetingId;
+    }
+
+    public void setMeetingId(String meetingId) {
+        this.meetingId = meetingId;
+    }
+
+    public List<String> getParticipantsEmailList() {
+        return participantsEmailList;
+    }
+
+    public void setParticipantsEmailList(List<String> participantsEmailList) {
+        this.participantsEmailList = participantsEmailList;
+    }
+
+    public String getTimeUTC() {
+        return timeUTC;
+    }
+
+    public void setTimeUTC(String timeUTC) {
+        this.timeUTC = timeUTC;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
