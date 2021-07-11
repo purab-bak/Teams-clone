b902c8a68c64e99e080951836b89795698301bb7
purab-bak <purab.bak@gmail.com>
2021-07-11 22:58:23 +0530

    Added Fadein/Fadeout transition

--- app/src/main/java/com/lite/teamsclone/Activities/LoginActivity.java
+++ app/src/main/java/com/lite/teamsclone/Activities/LoginActivity.java
@@ -71,6 +71,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         setContentView(R.layout.activity_login);
 
         init();
--- app/src/main/java/com/lite/teamsclone/Activities/SignUpActivity.java
+++ app/src/main/java/com/lite/teamsclone/Activities/SignUpActivity.java
@@ -66,6 +66,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         setContentView(R.layout.activity_sign_up);
 
         init();
