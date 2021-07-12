b4a503058b5bafaf2cd552debb3781aad4b3db49
purab-bak <purab.bak@gmail.com>
2021-07-12 21:03:54 +0530

    Removed dark theme

--- app/src/main/java/com/lite/teamsclone/Activities/SplashScreenActivity.java
+++ app/src/main/java/com/lite/teamsclone/Activities/SplashScreenActivity.java
@@ -1,6 +1,7 @@
 package com.lite.teamsclone.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -28,6 +29,8 @@ public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
