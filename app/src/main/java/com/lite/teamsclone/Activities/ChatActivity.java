8483770decdb407360a9fb79e8f6b0f747476f50
purab-bak <purab.bak@gmail.com>
2021-07-12 19:47:17 +0530

    UI done

--- app/src/main/java/com/lite/teamsclone/Activities/ChatActivity.java
+++ app/src/main/java/com/lite/teamsclone/Activities/ChatActivity.java
@@ -46,7 +46,6 @@ public class ChatActivity extends AppCompatActivity {
 
     FirebaseUser mCurrentUser;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
